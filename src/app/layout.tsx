627252defaa5a9b3b5607e1d8bcbc5216436d42c
Z User <z@container>
2026-06-26 17:25:15 +0000

    fca8c697-da88-4881-9a85-14ee9e208569

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/docs/theme-provider";
 import "./globals.css";
+import { SelectElementFab } from '@stsgs1980/fab-inspector';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <SelectElementFab />
       </body>
     </html>
   );
